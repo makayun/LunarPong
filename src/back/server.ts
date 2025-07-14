3f2b5a4ccd0ae1702de1cd8df20a55eed40d3607
bratzwitch <dudosslava@gmail.com>
2025-07-14 14:11:05 +0200

    trying some handles

--- src/back/server.ts
+++ src/back/server.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import { loadSecretsIntoEnv }			from './vault-loader';
+// import { loadSecretsIntoEnv }			from './vault-loader';
 
 import fs								from "node:fs";
 import path								from "node:path";
@@ -24,6 +24,8 @@ import authRoutes from '../auth/auth.routes';
 // import { initRedis } from './redis-client';
 
 async function main() {
+	// await loadSecretsIntoEnv("env/google");
+	// await loadSecretsIntoEnv("env/jwt");
 	await loadSecretsIntoEnv("env/google");
 	await loadSecretsIntoEnv("env/jwt");
 	await loadSecretsIntoEnv("env/ft");
